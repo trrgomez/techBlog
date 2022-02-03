f570bfe070a7f14012d80fb526af34ac55dcb03e
trgomez <88842062+trrgomez@users.noreply.github.com>
2022-02-03 13:40:37 -0600

    created a comment model and comment route

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,20 +1,33 @@
 const router = require("express").Router();
-const { Post, User } = require("../../models");
+const { Post, User, Comment } = require("../../models");
 
 // get all users
 router.get("/", (req, res) => {
   console.log("======================");
   Post.findAll({
-    // Query configuration
-    attributes: ["id", "title", "content", "created_at"],
-    order: [['created_at', 'DESC']], 
+    order: [['created_at', 'DESC']],
+    attributes: [
+      'id',
+      'title',
+      'content',
+      'created_at',
+    ],
     include: [
+      // include the Comment model here:
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
       {
         model: User,
-        attributes: ["username"],
-      },
-    ],
-  })
+        attributes: ['username']
+      }
+    ]
+   })
     .then((dbPostData) => res.json(dbPostData))
     .catch((err) => {
       console.log(err);
@@ -29,11 +42,20 @@ router.get("/:id", (req, res) => {
     },
     attributes: ["id", "content", "title", "created_at"],
     include: [
+      // include the Comment model here:
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
       {
         model: User,
-        attributes: ["username"],
-      },
-    ],
+        attributes: ['username']
+      }
+    ]
   })
     .then((dbPostData) => {
       if (!dbPostData) {
